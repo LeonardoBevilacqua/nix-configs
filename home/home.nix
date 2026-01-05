d3f32540e72c04d88fcccdf8954c213e56efffd6
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2026-01-05 13:33:21 -0300

    including neovim
    reusing same packages, may need refactor to favor programs.neovim.enable

--- home/home.nix
+++ home/home.nix
@@ -1,5 +1,10 @@
 { config, pkgs, ... }:
 
+let 
+    neovimConfig = import ../shells/dev-env/neovim.nix { inherit pkgs; };
+    devtools = import ../shells/dev-env/devtools.nix { inherit pkgs; };
+    languages = import ../shells/dev-env/languages.nix { inherit pkgs; };
+in
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
@@ -17,24 +22,7 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
+  home.packages = neovimConfig.packages ++ devtools ++ languages;
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
