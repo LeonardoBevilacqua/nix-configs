95adfd633d5195a2678340c18f2b8df9342f2b08
Leonardo Bevilacqua <leonardobevilacqua@CPQGESFG7M12YXHH.local>
2026-06-24 08:24:12 -0300

    including allacrity in mac config

--- home/flake.nix
+++ home/flake.nix
@@ -27,10 +27,17 @@
       homeConfigurations."leonardobevilacqua" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.aarch64-darwin;
         modules = [
-            {
+            ({ config, ... }: {
                 home.username = "leonardobevilacqua";
                 home.homeDirectory = "/Users/leonardobevilacqua";
-            }
+                programs.alacritty.enable = true;
+                xdg.configFile = {
+                  "alacritty" = {
+                      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/alacritty";
+                      recursive = true;
+                  };
+                };
+            })
             ./home.nix
         ];
       };
