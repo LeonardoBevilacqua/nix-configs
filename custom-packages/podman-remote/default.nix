d5b05901bfbd852413ee06292bbd56fbb6b1e2b6
Leonardo Bevilacqua <leonardobevilacqua@CPQGESFG7M12YXHH.local>
2026-07-23 08:58:36 -0300

    removing custom package for podman remote

--- custom-packages/podman-remote/default.nix
+++
@@ -1,7 +0,0 @@
-let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.11";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
-in
-{
-  podman-remote = pkgs.callPackage ./podman-remote.nix { };
-}
