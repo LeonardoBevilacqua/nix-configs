753d172c75596b6fe603d37c1cb14ab7353e44d8
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2026-09-22 13:09:32 -0300

    (bluesun-sway) settings GTK and QT apps to use dark theme

--- modules/home/user-leodev-bluesun.nix
+++ modules/home/user-leodev-bluesun.nix
@@ -26,5 +26,38 @@
                 recursive = true;
             };
         };
+
+        # dark theme in GTK and QT apps
+        dconf = {
+            enable = true;
+            settings = {
+                "org/gnome/desktop/interface" = {
+                    color-scheme = "prefer-dark";
+                    gtk-theme = "adw-gtk3-dark";
+                };
+            };
+        };
+        gtk = {
+            enable = true;
+            theme = {
+                name = "adw-gtk3-dark";
+                package = pkgs.adw-gtk3;
+            };
+            gtk3.extraConfig = {
+                Settings = ''gtk-application-prefer-dark-theme=1'';
+            };
+            gtk4.extraConfig = {
+              Settings = ''gtk-application-prefer-dark-theme=1'';
+            };
+        };
+        qt = {
+            enable = true;
+            platformTheme = "gnome";
+            style.name = "adwaita-dark";
+            style.package = pkgs.adwaita-qt;
+        };
+
+        # Set a session variable to reinforce the GTK theme for some apps
+        home.sessionVariables.GTK_THEME = "adw-gtk3-dark";
     };
 }
