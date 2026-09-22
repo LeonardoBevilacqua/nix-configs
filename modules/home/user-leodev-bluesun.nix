18a7e015011d17d8047c9f32c1467353f4527477
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2026-09-22 12:59:57 -0300

    (bluesun-sway) adding dotfiles for sway and waybar

--- modules/home/user-leodev-bluesun.nix
+++ modules/home/user-leodev-bluesun.nix
@@ -1,6 +1,10 @@
 {
     flake.modules.homeManager.leodev-bluesun = { config, pkgs, ... }:
 
+    let
+      dotfiles = "${config.home.homeDirectory}/dotfiles";
+      create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+    in
     {
         home.username = "leodev";
         home.homeDirectory = "/home/leodev";
@@ -10,6 +14,16 @@
             shellAliases = {
                 bluesun-rebuild = "sudo nixos-rebuild switch --flake ~/dotfiles/nix-configs/#nixos-bluesun";
                 bluesun-rebuild-sway = "sudo nixos-rebuild switch --flake ~/dotfiles/nix-configs/#nixos-bluesun --specialisation sway";
+            };
+        };
+        xdg.configFile = {
+            "sway" = {
+                source = create_symlink "${dotfiles}/sway";
+                recursive = true;
+            };
+            "waybar" = {
+                source = create_symlink "${dotfiles}/waybar";
+                recursive = true;
             };
         };
     };
