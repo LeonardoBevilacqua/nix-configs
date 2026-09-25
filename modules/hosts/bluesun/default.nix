32924a819778a6fc71dfcdf31476962018225cdb
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2026-09-25 14:18:23 -0300

    use `mkDriver` to build nvidia driver version 595.91.07

--- modules/hosts/bluesun/default.nix
+++ modules/hosts/bluesun/default.nix
@@ -37,15 +37,23 @@
     ];
 
     # GPU
-    # hardware.graphics = {
-    #   enable = true;
-    #   enable32Bit = true;
-    # };
-    # services.xserver.videoDrivers = [ "nvidia" ];
-    # hardware.nvidia = {
-    #     open = true;
-    #     modesetting.enable = true;
-    # };
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+    services.xserver.videoDrivers = [ "nvidia" ];
+    hardware.nvidia = {
+        open = true;
+        modesetting.enable = true;
+        package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+            version = "595.91.07";
+            sha256_64bit = "sha256-yiPIjdJLB6GRZE4eEc+3vN11NzBXSa9A+YABiwleYxM=";
+            sha256_aarch64 = "sha256-fqkN7ONFXtTeXyu2mQxorrk362Epxq3bz88hhKYQzwQ=";
+            openSha256 = "sha256-OB8Epd+qn/WywxsPiFpxEOAzlJqb6I1SyRoV3a8l71k=";
+            settingsSha256 = "sha256-QzT8Cw1luuZGP9DUje3HN/0ngiayqHURj+bqPsxlJ5w=";
+            persistencedSha256 = "sha256-3JQBaNmkwxvCXv9q8aHKas6VZM/JjLsuilC2t7ET0u0=";
+        };
+    };
 
     specialisation = {
       sway.configuration = {
@@ -56,9 +64,9 @@
         services.greetd.enable = lib.mkForce false;
         programs.steam.enable = lib.mkForce false;
         # Disables nvidia drivers, keeping the nouveau
-        # hardware.nvidia.open = lib.mkForce false;
-        # hardware.nvidia.modesetting.enable = lib.mkForce false;
-        # services.xserver.videoDrivers = lib.mkForce [ "modesetting" "nouveau" ];
+        hardware.nvidia.open = lib.mkForce false;
+        hardware.nvidia.modesetting.enable = lib.mkForce false;
+        services.xserver.videoDrivers = lib.mkForce [ "modesetting" "nouveau" ];
 
         programs.sway = {
           enable = true;
